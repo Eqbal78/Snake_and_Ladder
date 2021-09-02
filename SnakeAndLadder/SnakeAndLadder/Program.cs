ab74f5c7db4926fc2d9957c4f76dcd4c5d1c56f4
Eqbal78 <ahmadiqbal.a786@gmail.com>
2021-09-02 17:03:19 +0530

    Single player roll the die

--- SnakeAndLadder/SnakeAndLadder/Program.cs
+++ SnakeAndLadder/SnakeAndLadder/Program.cs
@@ -4,13 +4,25 @@ namespace SnakeAndLadder
 {
     class Program
     {
+
+        static int Roll_Dice()
+        {
+            Random r = new Random();
+            return r.Next(1, 7);
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Wellcome To Snake and Ladder game.");
+            int Start = 0;
+            int Finish = 100;
 
-            // Player Starting Position is zero
+            Console.WriteLine("Welcome To Snake and Ladder game.");
+            // player initial position
             int player_position = 0;
-            Console.WriteLine("player current position is " + player_position);
+            Console.WriteLine("Player current position is " + player_position);
+
+            // player rolls the dice and gets the value
+            int player_Roll_Dice = Roll_Dice();
+            Console.WriteLine("Player Dice value is " + player_Roll_Dice);
         }
     }
 }
