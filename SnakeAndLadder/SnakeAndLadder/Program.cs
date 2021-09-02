9f4a2efa7ebfe2ee3b878058ed13fe275eb24f1a
Eqbal78 <ahmadiqbal.a786@gmail.com>
2021-09-02 16:27:22 +0530

    Single player at Start position Zero

---
+++ SnakeAndLadder/SnakeAndLadder/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SnakeAndLadder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wellcome To Snake and Ladder game.");
+
+            // Player Starting Position is zero
+            int player_position = 0;
+            Console.WriteLine("player current position is " + player_position);
+        }
+    }
+}
